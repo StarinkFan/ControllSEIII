f3834e872ab0c2796bb569ca98f423105a99b69d
161250137 <161250137@mail.smal.nju.edu.cn>
2018-06-23 05:49:58 +0800

    完善controller

--- src/main/java/com/software2/demo/bean/Recharge.java
+++ src/main/java/com/software2/demo/bean/Recharge.java
@@ -4,9 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Entity;
-import javax.persistence.Id;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Date;
 
 @Data
@@ -16,6 +14,7 @@ import java.util.Date;
 @AllArgsConstructor
 public class Recharge {
     @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private int ID;
     private String uid;
     private String cardID;
